b54188174f4430edd4d1cc63d616b3873b507748
luketaylor5 <Luke.Taylor5@mail.bcu.ac.uk>
2025-03-12 12:44:42 +0000

    minor changes

--- Back-to-Back Project/Assets/Scripts/MenuController.cs
+++ Back-to-Back Project/Assets/Scripts/MenuController.cs
@@ -47,19 +47,19 @@ public class MenuController : MonoBehaviour
     //Loads the video for floor two for each house
     public void LoadLevysFloorTwo()
     {
-        SceneManager.LoadScene("Levy_V_FloorTwo");
+        SceneManager.LoadScene("Levy_V_SecondFloor");
     }
     public void LoadOldfieldsFloorTwo()
     {
-        SceneManager.LoadScene("Oldfields_V_FloorTwo");
+        SceneManager.LoadScene("Oldfields_V_SecondFloor");
     }
     public void LoadMitchellsFloorTwo()
     {
-        SceneManager.LoadScene("Mitchells_V_FloorTwo");
+        SceneManager.LoadScene("Mitchells_V_SecondFloor");
     }
     public void LoadGeorgeSaundersFloorTwo()
     {
-        SceneManager.LoadScene("GeorgeSaunders_V_FloorTwo");
+        SceneManager.LoadScene("GeorgeSaunders_V_SecondFloor");
     }
 
     //Loads the video for floor three for each house
